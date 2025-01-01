116f321ab1a12492fef513deb014ee3da47c40e9
PC-Jose-Home-Ubuntu <tijito4@gmail.com>
2025-01-01 16:42:42 +0100

    Actualizando

--- src/configs/config.js
+++ src/configs/config.js
@@ -3,6 +3,8 @@ export const API_URL = 'https://api.congeladosbrisamar.es/api/';
 
 export const API_URL_V1 = `${API_URL}v1/`;
 
+export const API_URL_V2 = `${API_URL}v1/`;
+
 export const LOGIN_FORM_LOGO = '/brisapp.svg'; 
 
 export const NAVBAR_LOGO = '/logos/blueapp-logo-horizontal.png';
