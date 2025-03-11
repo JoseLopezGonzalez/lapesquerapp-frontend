b2b37bf33885850af2a14678e9938aa1ee47a9a6
Jose-PCBRISAMAR <tijito4@gmail.com>
2025-03-11 20:43:50 +0100

    Actualizando

---
+++ src/services/taxService.js
@@ -0,0 +1,38 @@
+// /src/services/orderService.js
+
+import { API_URL_V2 } from "@/configs/config";
+
+
+
+
+
+/* getActiveOrders */
+export function getTaxOptions(token) {
+    return fetch(`${API_URL_V2}taxes/options`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`, // Enviar el token
+            'User-Agent': navigator.userAgent, // Incluye el User-Agent del cliente
+        },
+    })
+        .then((response) => {
+            if (!response.ok) {
+                return response.json().then((errorData) => {
+                    throw new Error(errorData.message || 'Error al obtener los impuestos');
+                });
+            }
+            return response.json();
+        })
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            // Aquí puedes agregar lógica adicional de logging o manejo global del error
+            throw error;
+        })
+        .finally(() => {
+            // Código a ejecutar independientemente del resultado (por ejemplo, limpiar loaders)
+            console.log('getTaxOptions finalizado');
+        });
+}
