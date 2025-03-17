ff2e543e94fb2a16652dff8347eef809b2b0f4fc
Jose-PCBRISAMAR <tijito4@gmail.com>
2025-03-17 15:38:49 +0100

    Actualizando

--- src/components/Shadcn/Dates/DatePicker/index.js
+++ src/components/Shadcn/Dates/DatePicker/index.js
@@ -24,8 +24,17 @@ const DatePicker = ({ value, onChange }) => {
                     mode="single"
                     selected={value}
                     onSelect={(date) => {
-                        onChange(date)
-                        setOpen(false)
+                        if (!date) return; // Por seguridad
+
+                        const year = date.getFullYear();
+                        const month = String(date.getMonth() + 1).padStart(2, '0'); // Mes es 0-indexado
+                        const day = String(date.getDate()).padStart(2, '0');
+
+                        const dateString = `${year}-${month}-${day}`;
+                        console.log(dateString); // Fecha en formato correcto yyyy-mm-dd
+
+                        onChange(dateString);
+                        setOpen(false);
                     }}
                     initialFocus
                 />
