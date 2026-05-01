87605583c74c0b1e0554c0720745c87d9b6e7da4
Jose-PCBRISAMAR <tijito4@gmail.com>
2026-05-01 13:50:07 +0200

    feat: add new product entry to export data for market analysis
    - Included a new product entry for "Domingo Cale" in the exportData.js file, enhancing the dataset available for market analysis.
    - Improved data integrity by expanding the list of available products for export.

--- src/components/Admin/MarketDataExtractor/ListadoComprasAsocPuntaDelMoral/exportData.js
+++ src/components/Admin/MarketDataExtractor/ListadoComprasAsocPuntaDelMoral/exportData.js
@@ -11,6 +11,7 @@ export const barcos = [
   { nombre: "Pepe Manuel", matricula: "3HU1420", cifArmador: "49056334W", codA3erp: "", codBrisapp: '14' },
   { nombre: 'Som do Mar', matricula: 'PTTVR-118920-L-1189', cifArmador: '', codA3erp: "", codBrisapp: '153' },
   { nombre: 'Diogo Miguel', matricula: 'PTPEN-113936-C-1139', cifArmador: '', codA3erp: "", codBrisapp: '148' },
+  { nombre: 'Domingo Cale', matricula: '1276', cifArmador: '', codA3erp: '', codBrisapp: '8' },
   { nombre: 'Hnos. Perez Rivero', matricula: 'HU-2-4-01', cifArmador: '', codA3erp: "", codBrisapp: '180' },
   /* Soledade */
   { nombre: 'Soledade', matricula: 'VR-427-L', cifArmador: '', codA3erp: '', codBrisapp: '191' },
